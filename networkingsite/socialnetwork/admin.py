3574e1c5e528c7dcd54765eba3befb23b699f927
Anjali <anjaleesingh14@gmail.com>
2024-06-06 22:06:07 +0530

    admin changes for friend request

--- networkingsite/socialnetwork/admin.py
+++ networkingsite/socialnetwork/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, FriendRequest
 from common.admin import BaseAdmin
 
 # Register your models here.
@@ -8,6 +8,10 @@ class RegisteredUserAdmin(BaseAdmin):
     list_filter = ["id", "email"]
     search_fields = ["id", "uuid", "email", "first_name"]
 
+class FriendRequestAdmin(BaseAdmin):
+    list_display = ["uuid", "from_user", "to_user", "status"]
+    list_filter = ["uuid", "from_user", "to_user", "status"]
+    search_fields = ["uuid", "from_user", "to_user", "status"]
 
 admin.site.register(User, RegisteredUserAdmin)
-
+admin.site.register(FriendRequest, FriendRequestAdmin)
